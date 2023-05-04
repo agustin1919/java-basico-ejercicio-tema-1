6596ce7ce2572d10cd59d25dbcdfa41b72dd331b
FacundoS <agustinsouyaux@gmail.com>
2023-05-04 12:23:59 -0300

    primer y ultimo commit de este ejercicio

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Main {
+    // Creacion de clase main y tipos de datos primitivos
+    public static void main(String[] args) {
+        // Enteros
+        byte var1= 1;
+        short var2= 10;
+        int var3= 50;
+        long var4= 100;
+
+        //Decimales
+        float var5 = 4.5f;
+        double var6 = 12.53526d;
+
+        // Booleanos
+        boolean t = true;
+        boolean f = false;
+
+        // texto
+        char a = 'a';
+        String nombre = "Facundo";
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+        System.out.println(var6);
+        System.out.println(t);
+        System.out.println(f);
+        System.out.println(a);
+        System.out.println(nombre);
+
+
+    }
+}
